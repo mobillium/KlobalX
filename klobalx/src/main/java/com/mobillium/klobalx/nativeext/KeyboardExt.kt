db3f5257f42605ec7adde791abb5320fdcd0e57a
moter <oter.mmurat@gmail.com>
2023-05-26 12:25:23 +0300

    Reformat keyboard extension

--- klobalx/src/main/java/com/mobillium/klobalx/nativeext/KeyboardExt.kt
+++ klobalx/src/main/java/com/mobillium/klobalx/nativeext/KeyboardExt.kt
@@ -2,33 +2,26 @@ package com.mobillium.klobalx.nativeext
 
 import android.app.Activity
 import android.content.Context
-import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
 
 
-fun Activity.hideKeyboard() {
-    val inputMethodManager = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
-    val focusedView = currentFocus ?: View(this)
-    inputMethodManager.hideSoftInputFromWindow(focusedView.windowToken, 0)
-}
+fun Activity.showKeyboard() =
+    WindowCompat.getInsetsController(window, window.decorView).show(WindowInsetsCompat.Type.ime())
 
-fun Fragment.hideKeyboard() {
-    val inputMethodManager = context?.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
-    val focusedView = activity?.currentFocus ?: View(context)
-    inputMethodManager.hideSoftInputFromWindow(focusedView.windowToken, 0)
-}
+fun Fragment.showKeyboard() = activity?.showKeyboard()
 
-fun EditText.hideKeyboard() {
-    clearFocus()
-    val inputMethodManager = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager?
-    inputMethodManager?.hideSoftInputFromWindow(windowToken, 0)
-}
+fun Activity.hideKeyboard() =
+    WindowCompat.getInsetsController(window, window.decorView).hide(WindowInsetsCompat.Type.ime())
 
+fun Fragment.hideKeyboard() = requireActivity().hideKeyboard()
 
 fun EditText.showKeyboard() {
     requestFocus()
-    val inputMethodManager = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager?
+    val inputMethodManager =
+        context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager?
     inputMethodManager?.showSoftInput(this, InputMethodManager.SHOW_IMPLICIT)
 }

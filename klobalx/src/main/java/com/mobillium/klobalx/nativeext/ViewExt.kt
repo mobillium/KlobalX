fa2d34decc30d6d7a5f0188b693001128c01733b
Abdulkerim Yıldırım <kermmyldrm@gmail.com>
2023-05-29 16:19:54 +0300

    Fix view height, width

--- klobalx/src/main/java/com/mobillium/klobalx/nativeext/ViewExt.kt
+++ klobalx/src/main/java/com/mobillium/klobalx/nativeext/ViewExt.kt
@@ -1,34 +1,28 @@
 package com.mobillium.klobalx.nativeext
 
-import android.content.Context
 import android.view.View
-import androidx.annotation.ColorRes
 import androidx.annotation.DimenRes
-import androidx.annotation.DrawableRes
-import androidx.core.content.ContextCompat
 
 fun View.visible() {
-    this.visibility = View.VISIBLE
+    visibility = View.VISIBLE
 }
 
 fun View.gone() {
-    this.visibility = View.GONE
+    visibility = View.GONE
 }
 
 fun View.invisible() {
-    this.visibility = View.INVISIBLE
+    visibility = View.INVISIBLE
 }
 
-fun View.width(@DimenRes width: Int) {
-    val layoutParams = this.layoutParams
-
-    layoutParams.width = context.resources.getDimensionPixelSize(width)
-    this.layoutParams = layoutParams
+fun View.width(@DimenRes widthRes: Int) {
+    with(layoutParams) {
+        width = context.resources.getDimensionPixelSize(widthRes)
+    }
 }
 
-fun View.height(@DimenRes height: Int) {
-    val layoutParams = this.layoutParams
-
-    layoutParams.height = context.resources.getDimensionPixelSize(height)
-    this.layoutParams = layoutParams
+fun View.height(@DimenRes heightRes: Int) {
+    with(layoutParams) {
+        height = context.resources.getDimensionPixelSize(heightRes)
+    }
 }

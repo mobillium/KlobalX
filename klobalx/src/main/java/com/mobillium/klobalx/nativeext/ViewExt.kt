ced83abbc73d47683438544d5d374490eae72d83
Abdulkerim Yıldırım <45524502+kerimisoffline@users.noreply.github.com>
2023-06-02 16:15:27 +0300

    Add view height, width extensions (#5)

--- klobalx/src/main/java/com/mobillium/klobalx/nativeext/ViewExt.kt
+++ klobalx/src/main/java/com/mobillium/klobalx/nativeext/ViewExt.kt
@@ -1,15 +1,28 @@
 package com.mobillium.klobalx.nativeext
 
 import android.view.View
+import androidx.annotation.DimenRes
 
 fun View.visible() {
-    this.visibility = View.VISIBLE
+    visibility = View.VISIBLE
 }
 
 fun View.gone() {
-    this.visibility = View.GONE
+    visibility = View.GONE
 }
 
 fun View.invisible() {
-    this.visibility = View.INVISIBLE
+    visibility = View.INVISIBLE
+}
+
+fun View.width(@DimenRes widthRes: Int) {
+    with(layoutParams) {
+        width = context.resources.getDimensionPixelSize(widthRes)
+    }
+}
+
+fun View.height(@DimenRes heightRes: Int) {
+    with(layoutParams) {
+        height = context.resources.getDimensionPixelSize(heightRes)
+    }
 }

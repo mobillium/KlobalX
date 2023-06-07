6c60236e1cb7ca40d6b8daff76e19030767fc7c8
Tamer Sarıoğlu <tamerthedark@hotmail.com>
2023-06-07 13:07:10 +0300

    Refactor code for PR comments

--- klobalx/src/main/java/com/mobillium/klobalx/nativeext/FragmentExt.kt
+++ klobalx/src/main/java/com/mobillium/klobalx/nativeext/FragmentExt.kt
@@ -3,6 +3,6 @@ package com.mobillium.klobalx.nativeext
 import androidx.fragment.app.Fragment
 import com.google.android.material.snackbar.Snackbar
 
-fun Fragment.showSnackBar(snackBarText: String, timeLength: Int) {
-    activity?.let { Snackbar.make(it.findViewById(android.R.id.content), snackBarText, timeLength).show() }
+fun Fragment.showSnackBar(snackBarText: String) {
+    requireActivity().let { Snackbar.make(it.findViewById(android.R.id.content), snackBarText, 2).show() }
 }

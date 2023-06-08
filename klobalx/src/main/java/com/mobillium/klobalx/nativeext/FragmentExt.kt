5cc288e7e309d2db945609869fe75aa6e00d56f2
Akın Demir <akin.demir@mobillium.com>
2023-06-08 17:29:02 +0300

    Add default value for timeLength

--- klobalx/src/main/java/com/mobillium/klobalx/nativeext/FragmentExt.kt
+++ klobalx/src/main/java/com/mobillium/klobalx/nativeext/FragmentExt.kt
@@ -3,6 +3,8 @@ package com.mobillium.klobalx.nativeext
 import androidx.fragment.app.Fragment
 import com.google.android.material.snackbar.Snackbar
 
-fun Fragment.showSnackBar(snackBarText: String) {
-    requireActivity().let { Snackbar.make(it.findViewById(android.R.id.content), snackBarText, 2).show() }
+fun Fragment.showSnackBar(snackBarText: String, timeLength: Int = Snackbar.LENGTH_SHORT) {
+    requireActivity().let {
+        Snackbar.make(it.findViewById(android.R.id.content), snackBarText, timeLength).show()
+    }
 }

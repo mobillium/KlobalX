508b6cba1533b683f0294996afd27d0a0fea8492
Akın Demir <akin.demir@mobillium.com>
2023-09-12 21:48:23 +0300

    Add toast extension for fragment and refactor extension function naming

--- klobalx/src/main/java/com/mobillium/klobalx/nativeext/FragmentExt.kt
+++ klobalx/src/main/java/com/mobillium/klobalx/nativeext/FragmentExt.kt
@@ -1,5 +1,6 @@
 package com.mobillium.klobalx.nativeext
 
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.google.android.material.snackbar.Snackbar
 
@@ -7,4 +8,14 @@ fun Fragment.showSnackBar(snackBarText: String, timeLength: Int = Snackbar.LENGT
     requireActivity().let {
         Snackbar.make(it.findViewById(android.R.id.content), snackBarText, timeLength).show()
     }
+}
+
+/**
+ * Displays a toast message on the screen.
+ *
+ * @param message The text message to be displayed in the toast.
+ * @param duration Optional duration of the toast.
+ */
+fun Fragment.showToast(message: String, duration: Int = Toast.LENGTH_SHORT) {
+    Toast.makeText(requireContext(), message, duration).show()
 }
